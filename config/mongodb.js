4e9a891b922eac759c9d0f46733eff7aa852c184
Saqib Abdullah Mustafy <89904640+saqibam92@users.noreply.github.com>
2024-03-21 21:32:03 +0600

    Update mongodb.js

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,7 +3,7 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log(
       `mongodb connected ${mongoose.connection.host}`.green.underline
     );
